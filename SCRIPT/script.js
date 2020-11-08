ebbc999de7f1f3f4e4f20391921bee761e1a67cd
alexandre-bousquet <alexandre.bousquet01@etu.umontpellier.fr>
2020-11-08 01:05:07 +0100

    Déplacement des scripts TEST

---
+++ SCRIPT/script.js
@@ -0,0 +1,93 @@
+window.onscroll = function() {
+	scrollFunction()
+};
+
+function scrollFunction() {
+	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+		document.getElementById("TOP").style.display = "block";
+	} else {
+		document.getElementById("TOP").style.display = "none";
+	}
+
+	// Fusion de fonctions
+	var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+	var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+	var scrolled = (winScroll / height) * 100;
+	document.getElementById("myBar").style.width = scrolled + "%";
+	document.getElementById("myTopnav").style.backgroundSize = scrolled + "%";
+	//document.getElementById("myTopnav").style.backgroundColor = "blue";
+}
+
+function topFunction() {
+	document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
+}
+
+window.addEventListener("beforeunload", function () {
+	var animation = document.getElementsByClassName("corps")[0];
+	animation.classList.add("animate-out");
+	//document.body.classList.add("animate-out");
+});
+
+var modal01 = document.getElementById("myModal01");
+var img01 = document.getElementById("myImg01");
+var img02 = document.getElementById("myImg02");
+var img03 = document.getElementById("myImg03");
+var img04 = document.getElementById("myImg04");
+var modalImg01 = document.getElementById("img01");
+var modalImg02 = document.getElementById("img02");
+var modalImg03 = document.getElementById("img03");
+var modalImg04 = document.getElementById("img04");
+var captionText = document.getElementById("caption");
+
+img01.onclick = function() {
+	modal01.style.display = "block";
+	modalImg01.src = this.src;
+	captionText.innerHTML = this.alt;
+}
+
+img02.onclick = function() {
+	modal01.style.display = "block";
+	modalImg01.src = this.src;
+	captionText.innerHTML = this.alt;
+}
+
+img03.onclick = function() {
+	modal01.style.display = "block";
+	modalImg01.src = this.src;
+	captionText.innerHTML = this.alt;
+}
+
+img04.onclick = function() {
+	modal01.style.display = "block";
+	modalImg01.src = this.src;
+	captionText.innerHTML = this.alt;
+}
+
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() { 
+	modal01.style.display = "none";
+}
+
+function myFunction() {
+	var x = document.getElementById("myTopnav");
+	if (x.className === "topnav") {
+		x.className += " responsive";
+	} else {
+		x.className = "topnav";
+	}
+}
+
+var lc = document.getElementById("littleCard1");
+var bc = document.getElementById("bigCard1");
+bc.onmouseover = function() {myFuncionCardHoverON()};
+bc.onmouseout = function() {myFuncionCardHoverOFF()};
+
+function myFuncionCardHoverON() {
+	bc.classList.add("bigCardAll");
+}
+
+function myFuncionCardHoverOFF() {
+	bc.classList.remove("bigCardAll");
+}
