382fdef9ef8fff9c226f32ee8f5cdad2740bcb66
alexandre-bousquet <alexandre.bousquet01@etu.umontpellier.fr>
2021-10-06 10:29:02 +0200

    Display an information about the future of this website

--- SCRIPT/script.js
+++ SCRIPT/script.js
@@ -1,32 +1,32 @@
-window.onscroll = function() {
-	scrollFunction()
+window.onscroll = function () {
+    scrollFunction()
 };
 
 function scrollFunction() {
-	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-		document.getElementById("TOP").style.display = "block";
-	} else {
-		document.getElementById("TOP").style.display = "none";
-	}
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("TOP").style.display = "block";
+    } else {
+        document.getElementById("TOP").style.display = "none";
+    }
 
-	// Fusion de fonctions
-	let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-	let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-	let scrolled = (winScroll / height) * 100;
-	document.getElementById("myBar").style.width = scrolled + "%";
-	document.getElementById("myTopnav").style.backgroundSize = scrolled + "%";
-	//document.getElementById("myTopnav").style.backgroundColor = "blue";
+    // Fusion de fonctions
+    let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let scrolled = (winScroll / height) * 100;
+    document.getElementById("myBar").style.width = scrolled + "%";
+    document.getElementById("myTopnav").style.backgroundSize = scrolled + "%";
+    //document.getElementById("myTopnav").style.backgroundColor = "blue";
 }
 
 function topFunction() {
-	document.body.scrollTop = 0;
-	document.documentElement.scrollTop = 0;
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
 }
 
 window.addEventListener("beforeunload", function () {
-	let animation = document.getElementsByClassName("corps")[0];
-	animation.classList.add("animate-out");
-	//document.body.classList.add("animate-out");
+    let animation = document.getElementsByClassName("corps")[0];
+    animation.classList.add("animate-out");
+    //document.body.classList.add("animate-out");
 });
 
 let modal01 = document.getElementById("myModal01");
@@ -34,49 +34,53 @@ let modalImg01 = document.getElementById("img01");
 let captionText = document.getElementById("caption");
 
 for (let i = 0; i < 2; i++) {
-	//for (let i = 0; i < 4; i++) {
-	document.getElementById("myImg0" + (i + 1)).onclick = function() {
-		modal01.style.display = "block";
-		modalImg01.src = this.src;
-		captionText.innerHTML = this.alt;
-	}
+    //for (let i = 0; i < 4; i++) {
+    document.getElementById("myImg0" + (i + 1)).onclick = function () {
+        modal01.style.display = "block";
+        modalImg01.src = this.src;
+        captionText.innerHTML = this.alt;
+    }
 }
 
 let span = document.getElementsByClassName("close")[0];
 
-span.onclick = function() {
-	modal01.style.display = "none";
+span.onclick = function () {
+    modal01.style.display = "none";
 }
 
 function myFunction() {
-	let x = document.getElementById("myTopnav");
-	if (x.className === "topnav") {
-		x.className += " responsive";
-	} else {
-		x.className = "topnav";
-	}
+    let x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
 }
 
 let lc = document.getElementById("littleCard1");
 let bc = document.getElementById("bigCard1");
-bc.onmouseover = function() {myFuncionCardHoverON()};
-bc.onmouseout = function() {myFuncionCardHoverOFF()};
+bc.onmouseover = function () {
+    myFuncionCardHoverON()
+};
+bc.onmouseout = function () {
+    myFuncionCardHoverOFF()
+};
 
 function myFuncionCardHoverON() {
-	bc.classList.add("bigCardAll");
+    bc.classList.add("bigCardAll");
 }
 
 function myFuncionCardHoverOFF() {
-	bc.classList.remove("bigCardAll");
+    bc.classList.remove("bigCardAll");
 }
 
 function changePage(page) {
-	let xhr = new XMLHttpRequest();
-	xhr.open("GET", "https://alexandre-bousquet.github.io/HTML/" + page + ".html", true)
-	xhr.send(null);
-	console.log(xhr);
-	console.log(xhr.responseText);
-	return xhr.responseText;
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://alexandre-bousquet.github.io/HTML/" + page + ".html", true)
+    xhr.send(null);
+    console.log(xhr);
+    console.log(xhr.responseText);
+    return xhr.responseText;
 }
 
 /*function getBody(page) {
